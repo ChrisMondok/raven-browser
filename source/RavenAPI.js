be31f62d2320888ab8588a79651efc89bf1f9d7e
Chris Mondok <cmondok@marathondata.com>
2013-05-07 14:13:10 -0400

    Tidied up Raven API

--- source/RavenAPI.js
+++ source/RavenAPI.js
@@ -4,20 +4,19 @@ enyo.kind({
 	published:{
 		ravenHost: "mds-d77gds1",
 		ravenPort: 8080,
-		timeout:30000
+		timeout: 30000
 	},
 	events:{
 		onConnectionChanged:"",
 	},
 	getTenants:function(callback, errorCallback) {
 		var tenants = [];
-		var ajax = new enyo.Ajax({url:this.getRavenUrl()+"databases", timeout:this.getTimeout()});
-		ajax.go()
-		ajax.response(this,function(sender,response) {
-			callback(response);
-		});
-		if(errorCallback)
-			ajax.error(errorCallback);
+		new enyo.Ajax({url:this.getRavenUrl()+"databases", timeout:this.getTimeout()})
+			.go()
+			.response(this,function(sender,response) {
+				callback(response);
+			})
+			.error(errorCallback);
 	},
 	getDocuments:function(tenantId, callback, progressCallback, errorCallback) {
 		return this.loadInMultipleRequests(
@@ -29,16 +28,14 @@ enyo.kind({
 		);
 	},
 	loadDocument:function(tenantId, documentId, callback, errorCallback) {
-		var ajax = new enyo.Ajax({
-			url:this.getRavenUrl()+"databases/"+tenantId+"/indexes/Raven/DocumentsByEntityName",
-		});
-		ajax.go({query:"__document_id:"+documentId});
-		ajax.response(function(ajax,response) {
-			if(response.Results.length != 1)
-				return errorCallback({error:"Got "+response.Results.length+" results."});
-			callback(ajax,response.Results[0]);	
-		});
-		ajax.error(errorCallback);
+		new enyo.Ajax({ url:this.getRavenUrl()+"databases/"+tenantId+"/indexes/Raven/DocumentsByEntityName" })
+			.go({query:"__document_id:"+documentId})
+			.response(function(ajax,response) {
+				if(response.Results.length != 1)
+					return errorCallback({error:"Got "+response.Results.length+" results."});
+				callback(ajax,response.Results[0]);	
+			})
+			.error(errorCallback);
 	},
 	saveDocument:function(tenantId, documentId, value, callback, errorCallback) {
 		if(value.hasOwnProperty('@metadata') && value['@metadata']['Raven-Entity-Name'])
@@ -46,44 +43,21 @@ enyo.kind({
 		else
 			headers = {};
 
-		var ajax = new enyo.Ajax({
+		new enyo.Ajax({
 			method:"PUT",
 			url:this.getRavenUrl()+"databases/"+tenantId+"/docs/"+documentId,
 			contentType:"application/json",
 			cacheBust:false,
 			postBody:value,
 			headers:headers
-		});
-
-		ajax.go();
-
-		ajax.response(callback);
-		ajax.error(errorCallback);
-	},
-	createDocument:function(tenantId, documentId, callback, errorCallback) {
-		var ajax = new enyo.Ajax({
-			method:"PUT",
-			url:this.getRavenUrl()+"databases/"+tenantId+"/docs/"+documentId,
-			contentType:"application/json",
-			cacheBust:false,
-			postBody:{data:"goes here"}
-		});
-
-		ajax.go();
-
-		ajax.response(callback);
-		ajax.error(errorCallback);
+		}).go().response(callback).error(errorCallback);
 	},
 	deleteDocument:function(tenantId, documentId, callback, errorCallback) {
-		var ajax = new enyo.Ajax({
+		new enyo.Ajax({
 			method:"DELETE",
 			url:this.getRavenUrl()+"databases/"+tenantId+"/docs/"+documentId,
 			cacheBust:false,
-		});
-		ajax.go();
-
-		ajax.response(callback);
-		ajax.error(errorCallback);
+		}).go().response(callback).error(errorCallback);
 	},
 	loadInMultipleRequests:function(url,params,callback,progressCallback,errorCallback) {
 		var loader = {
