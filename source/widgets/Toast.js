f34fddfa217fa64af781cfb44665892cc2acbd5b
Chris Mondok <cmondok@marathondata.com>
2013-11-11 10:52:30 -0500

    Stack toasts

--- source/widgets/Toast.js
+++ source/widgets/Toast.js
@@ -5,20 +5,38 @@ enyo.kind({
 	floating:true,
 	centered:true,
 	published:{
-		lifetime:2000,
+		lifetime:5000,
 	},
 	handlers:{
-		ontap:"hide",
+		ontap:"destroy",
 	},
+
+	statics:{
+		instances: [],
+		positionInstances:function() {
+			var bottom = 6*16;
+			for(var i = 0; i < this.instances.length; i++)
+			{
+			   var toast = this.instances[this.instances.length - (i+1)];
+			   toast.setBounds({bottom:bottom});
+			   bottom += 2 + toast.getBounds().height;
+			}
+		}
+	},
+
 	create:function() {
 		var r = this.inherited(arguments);
 		this.show();
-		setTimeout(enyo.bind(this,"destroyToast"),this.getLifetime());
+		setTimeout(enyo.bind(this,"destroy"),this.getLifetime());
 		this.applyStyle('top','');
-		this.applyStyle('bottom','8em');
+		this.applyStyle('bottom','0em');
+		onyx.Toast.instances.push(this);
+		onyx.Toast.positionInstances();
 		return r;
 	},
-	destroyToast:function() {
-		this.destroy();
+	destroy:function() {
+		onyx.Toast.instances.splice(onyx.Toast.instances.indexOf(this),1);
+		this.inherited(arguments);
+		onyx.Toast.positionInstances();
 	}
 });
