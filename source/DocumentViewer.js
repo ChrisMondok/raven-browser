d0d4469d441fd7827c1453ac292bf94b8f24e4f9
Chris Mondok <cmondok@marathondata.com>
2013-11-12 13:17:58 -0500

    Use MoreToolbar for document viewer toolbar
    I'm not so sure about this one

--- source/DocumentViewer.js
+++ source/DocumentViewer.js
@@ -35,30 +35,28 @@ enyo.kind({
 					{kind:"FittableRows", classes:"enyo-fit", controlClasses:"nice-margin", components:[
 						{kind:"onyx.Groupbox", components:[
 							{kind:"onyx.InputDecorator", style:"display:block", components:[
-								{name:"entityNameInput", kind:"onyx.Input", style:"width:100%", placeholder:"Raven-Entity-Name"},
+								{name:"entityNameInput", kind:"onyx.Input", style:"width:100%", placeholder:"Raven-Entity-Name"}
 							]},
 							{kind:"onyx.InputDecorator", style:"display:block", components:[
-								{name:"clrTypeInput", kind:"onyx.Input", style:"width:100%", placeholder:"Raven-Clr-Type"},
-							]},
+								{name:"clrTypeInput", kind:"onyx.Input", style:"width:100%", placeholder:"Raven-Clr-Type"}
+							]}
 						]},
 						{kind:"Scroller", fit:true, components:[
 							{name:"metadataDisplay", showing:true, tag:"pre", style:"margin:0"}
 						]}
-					]},
-				]},
+					]}
+				]}
 		]},
 		{style:"z-index:2; position:relative;", components:[
-			{kind:"onyx.Toolbar", components:[
-				{kind:"FittableColumns", style:"width:100%", components:[
-					{kind:"onyx.InputDecorator", fit:true, components:[
-						{name:"documentIdInput", kind:"onyx.Input", style:"width:100%", onchange:"documentIdInputChanged"}
-					]},
-					{name:"reloadButton", kind:"onyx.Button", content:"Load", ontap:"loadDocument", disabled:true},
-					{name:"metadataButton", kind:"onyx.Button", content:"Metadata", ontap:"showMetadata"},
-					{name:"saveButton", kind:"onyx.Button", classes:"onyx-affirmative", content:"Save", ontap:'saveDocument', disabled:true},
+			{kind:"onyx.MoreToolbar", components:[
+				{kind:"onyx.InputDecorator", fit:true, style:"min-width:200px", unmoveable:true, components:[
+					{name:"documentIdInput", kind:"onyx.Input", style:"width:100%", onchange:"documentIdInputChanged"}
 				]},
-			]},
-		]},
+				{name:"reloadButton", kind:"onyx.Button", content:"Load", ontap:"loadDocument", disabled:true},
+				{name:"metadataButton", kind:"onyx.Button", content:"Metadata", ontap:"showMetadata"},
+				{name:"saveButton", kind:"onyx.Button", classes:"onyx-affirmative", content:"Save", unmoveable:true, ontap:'saveDocument', disabled:true}
+			]}
+		]}
 	],
 	loadDocument:function() {
 		this.getApi().loadDocument(this.getTenantId(),this.getDocumentId())
@@ -99,6 +97,7 @@ enyo.kind({
 				break;
 			default:
 				this.doErrorReceived({error:"Failed to load document"});
+				break;
 			}
 		}
 	},
